28914147037a7c6988f8940d12ed79ddf4aa8e8e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2025-05-01 17:40:23 -0300

    first commit

---
+++ src/VimScroll.tsx
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+
+let startedJAt = 0;
+let startedKAt = 0;
+let lastScroll = 0;
+let inCooldown = false;
+
+const VimScroll = ({
+  cooldown = 300,
+  scrollBy = 150,
+  ignoreOn = ["input", "textarea"],
+}: {
+  cooldown?: number;
+  scrollBy?: number;
+  ignoreOn?: string[];
+}) => {
+  useEffect(() => {
+    const onKeyPressed = (evt: KeyboardEvent) => {
+      if (evt.key === "j") {
+        startedJAt = Date.now();
+      }
+
+      if (evt.key === "k") {
+        startedKAt = Date.now();
+      }
+    };
+
+    const onKeyDown = (evt: KeyboardEvent) => {
+      const activeElement = document.activeElement?.tagName.toLowerCase();
+
+      if (activeElement && ignoreOn.includes(activeElement)) return;
+
+      if (inCooldown) {
+        if (Date.now() - lastScroll > cooldown) {
+          inCooldown = false;
+        } else {
+          return;
+        }
+      }
+
+      if (evt.key === "j") {
+        evt.preventDefault();
+        window.scrollBy({ top: scrollBy, behavior: "smooth" });
+        lastScroll = Date.now();
+        inCooldown = true;
+      }
+
+      if (evt.key === "k") {
+        evt.preventDefault();
+        window.scrollBy({ top: -scrollBy, behavior: "smooth" });
+        lastScroll = Date.now();
+        inCooldown = true;
+      }
+    };
+
+    addEventListener("keypress", onKeyPressed);
+    addEventListener("keydown", onKeyDown);
+
+    return () => {
+      removeEventListener("keypress", onKeyPressed);
+      removeEventListener("keydown", onKeyDown);
+    };
+  }, [cooldown]);
+
+  return null;
+};
+
+export default VimScroll;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as VimScroll } from "./VimScroll.tsx";
